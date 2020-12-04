424ed548dbe63599af9c4407c6687bc33e5f1786
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2020-12-04 12:45:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 /* Config 2 */
-var headerImage = 'https://i.imgur.com/mzlmMAy.png';
-var footer = 'http://sadps.com/apply';
-/* ------------------------------- */
-/* ext no touchy touchy */
 var headerImage = 'https://i.imgur.com/SqJCooD.png';
 var footer = 'Footer Note  or URL';
-
+/* ------------------------------- */
+/* ext no touchy touchy */
+document.getElementById('footer').innerHTML = footer;
+document.getElementById('headerImage').src = headerImage;
 /* Need Help? Join my discord @ discord.gg/yWddFpQ */
